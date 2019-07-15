6b9bda63469a1c8c2b51b7c578cb6a861a911191
BlockChainDev <kevtheappdev@gmail.com>
2019-07-15 19:00:40 +0100

    [keys]
    - Add method to check for duplicates

--- src/keys.rs
+++ src/keys.rs
@@ -1,8 +1,9 @@
 use crate::constants::BASEPOINT;
-use curve25519_dalek::ristretto::RistrettoPoint;
+use curve25519_dalek::ristretto::{CompressedRistretto, RistrettoPoint};
 use curve25519_dalek::scalar::Scalar;
 use sha2::Sha512;
 
+use std::collections::HashSet;
 // Public key set represents a set of public keys
 // note that this is not a `tuple`. A tuple allows duplicates while a set
 // does not. While this is not a limitation placed upon the protocol by the
@@ -17,6 +18,21 @@ impl PublicSet {
     // Returns the number of public keys in the set
     pub fn len(&self) -> usize {
         self.0.len()
+    }
+    // Checks if the public set contains any duplicate keys
+    pub fn duplicates_exist(&self) -> bool {
+        // XXX: Very in-efficient way to do this.
+        // We can wait for upstream crate to implement Hash and use a HashSet instead
+
+        let compressed_points: Vec<CompressedRistretto> =
+            self.0.iter().map(|point| point.compress()).collect();
+
+        let hashable_slice: Vec<&[u8; 32]> =
+            compressed_points.iter().map(|cp| cp.as_bytes()).collect();
+
+        let uniques: HashSet<_> = hashable_slice.iter().collect();
+
+        self.0.len() != uniques.len()
     }
 }
 
@@ -95,6 +111,20 @@ mod test {
                 _ => panic!("could not get the private/public key at index {} ", i),
             }
         }
+    }
+    #[test]
+    fn check_duplicates_exist() {
+        let private_set = generate_private_set(10);
+        let mut public_set = private_set.to_public_set();
+
+        let dup_exists = public_set.duplicates_exist();
+        assert!(!dup_exists);
+
+        let last_element = public_set.0.last().unwrap().clone();
+        public_set.0[0] = last_element;
+
+        let dup_exists = public_set.duplicates_exist();
+        assert!(dup_exists);
     }
 
 }
