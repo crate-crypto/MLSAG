a4ccc7139491176da331985e8dcf391e4b8a3fc6
BlockChainDev <kevtheappdev@gmail.com>
2019-07-15 13:55:37 +0100

    [keys]
    - Keys are an abstraction over a vector of points/scalars. They are a set of points/scalars with methods attached to them

---
+++ src/keys.rs
@@ -0,0 +1,100 @@
+use crate::constants::BASEPOINT;
+use curve25519_dalek::ristretto::RistrettoPoint;
+use curve25519_dalek::scalar::Scalar;
+use sha2::Sha512;
+
+// Public key set represents a set of public keys
+// note that this is not a `tuple`. A tuple allows duplicates while a set
+// does not. While this is not a limitation placed upon the protocol by the
+// maths, placing duplicated keys into this vector is akin
+// to proving that you own the same key twice. This restriction will be placed
+// onto the protocol at this level, as the author cannot think of a
+// context where proving you own the same key twice would be useful.
+#[derive(Debug, Clone)]
+pub struct PublicSet(pub Vec<RistrettoPoint>);
+
+impl PublicSet {
+    // Returns the number of public keys in the set
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct PrivateSet(pub(crate) Vec<Scalar>);
+
+impl PrivateSet {
+    pub fn new(scalars: Vec<Scalar>) -> Self {
+        PrivateSet(scalars)
+    }
+    // Takes a set of private keys
+    // and returns the corresponding public key set
+    pub fn to_public_set(&self) -> PublicSet {
+        let public_keys = self
+            .0
+            .iter()
+            .map(|&x| x * BASEPOINT)
+            .collect::<Vec<RistrettoPoint>>();
+
+        PublicSet(public_keys)
+    }
+
+    // Returns all of the keyImages for a specific private key set
+    // We calculate the key image using the formula keyImage = privateKey * HashToPoint(PublicKey)
+    // Note that the HashToPoint must not allow the basepoint in the public key to be factored out
+    pub fn compute_key_images(&self, public_set: &PublicSet) -> Vec<RistrettoPoint> {
+        // Set of private keys must be the same length as the set of private keys
+        assert_eq!(self.len(), public_set.len());
+
+        let num_public_keys = public_set.len();
+
+        let mut key_images: Vec<RistrettoPoint> = Vec::with_capacity(num_public_keys);
+
+        for i in 0..num_public_keys {
+            let public_key_i = public_set.0[i].compress();
+            let private_key_i = self.0[i];
+
+            let hashed_public_key =
+                RistrettoPoint::hash_from_bytes::<Sha512>(public_key_i.as_bytes());
+
+            let key_image = private_key_i * hashed_public_key;
+
+            key_images.push(key_image);
+        }
+
+        key_images
+    }
+
+    // Returns the number of private keys in the set
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::tests_helper::*;
+    // This test is a sanity check for private to public key sets.
+    // The iter method is used when converting from a set of private keys
+    // to a set of public keys. In the test, we use a for loop and check that both
+    // are equal.
+    #[test]
+    fn private_set_to_public_set() {
+        let private_set = generate_private_set(10);
+        let public_set = private_set.to_public_set();
+
+        assert_eq!(private_set.len(), public_set.len());
+
+        for i in 0..private_set.len() {
+            match (private_set.0.get(i), public_set.0.get(i)) {
+                (Some(private_key), Some(expected_public_key)) => {
+                    let public_key = private_key * &BASEPOINT;
+                    assert_eq!(public_key, *expected_public_key);
+                }
+                _ => panic!("could not get the private/public key at index {} ", i),
+            }
+        }
+    }
+
+}
